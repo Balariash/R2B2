e30ad8f34858107f0ae8c3953f10bf7bddec221a
Jordi-Menorca <106823282+Jordi-Menorca@users.noreply.github.com>
2022-06-07 11:32:54 +0200

    Update config.py

--- python/config.py
+++ python/config.py
@@ -1,2 +1,2 @@
-host = "169.254.10.124" # IP del dispositiu ha on es localitza el broker MQTT
-#host = 'localhost'     # IP si el broker es troba en el mateix dipositiu 
+host = "169.254.10.124" # IP of the broker MQTT device.
+#host = 'localhost'     # Local broker.
